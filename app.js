0545b70a457fe39be4e3508023bbf7e6ff000106
Franklin Esquivel <franklin.esquivel@outlook.com>
2017-09-16 21:18:05 -0600

    First Setup 😊😊
    The principal styles, functionality and workflow :tongue:

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const path = require('path');
+
+const hostname = 'localhost';
+const port = 3000;
+const app = express();
+
+app.get('/', (req, res) => {
+	res.statusCode = 200;
+	res.setHeader('Content-Type', 'text/html');
+	res.sendFile(path.join(__dirname, 'public'));
+})
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.listen(port, hostname, () => {
+	console.log(`Server running at http://${hostname}:${port}`);
+});
