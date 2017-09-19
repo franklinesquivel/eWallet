1950a120f415cc1d835fb489011db78c50836417
Franklin Esquivel <franklin.esquivel@outlook.com>
2017-09-18 21:48:51 -0600

    Validation v.1 :heart:
    Basic structure and iteration of user input data validation based on Objects(Key => Value)

--- public/js/init.js
+++ public/js/init.js
@@ -1,4 +1,70 @@
 (function(){
+	const CustomForm = function(element) {
+		this.element = element;
+	};
+
+	CustomForm.prototype.validate = function(dataset) {
+		let f = 0, options = dataset.rules;
+		for(el in options){
+			if (this.element[el] !== undefined) {
+				for(rule in options[el]){
+					let msg = options[el][rule].msg,
+						pattern = msg !== undefined ? options[el][rule].value : undefined,
+						condition;
+
+					switch(rule){
+						case "required":
+							condition = parseInt(this.element[el].value.trim().length) === 0;
+							break;
+						case "pattern":
+							condition = !pattern.test(this.element[el].value);
+							break;
+					}
+					condition ? f++ : "";
+					this.element[el].custom.validateInput(condition, msg);
+				}
+			}else{
+				console.warn(`El elemento ${el} no existe en el DOM!`);
+			}
+		}
+
+		f == 0 ? dataset.success() : dataset.invalid();
+	}
+
+	Object.defineProperty(HTMLFormElement.prototype, "custom", {
+		get: function () {
+			Object.defineProperty(this, "custom", {
+				value: new CustomForm(this)
+			});
+			
+			return this.custom;
+		},
+		configurable: true,
+		writeable: false
+	});
+
+	const CustomInput = function( element ){
+		this.element = element;
+	}
+
+	CustomInput.prototype.validateInput = function(toogle, msg = undefined){
+		toogle ? this.element.classList.add('invalid') : this.element.classList.remove('invalid');
+		// toogle ? txt.classList.remove('valid') : txt.classList.add('valid');
+		msg !== undefined && toogle ? console.log(msg) : "";
+	}
+
+	Object.defineProperty(HTMLInputElement.prototype, "custom", {
+		get: function () {
+			Object.defineProperty(this, "custom", {
+				value: new CustomInput(this)
+			});
+			
+			return this.custom;
+		},
+		configurable: true,
+		writeable: false
+	});
+
 	const input_selector = 'input[type=text], input[type=password], input[type=email], input[type=url], input[type=tel], input[type=number], input[type=date], input[type=search], textarea'.split(',');
 	document.addEventListener('DOMContentLoaded', () => {
 		input_selector.forEach(i => {
@@ -16,6 +82,41 @@
 		});
 
 		window.updateTextFields();
+		
+		frmRegister.custom.validate({
+			"rules": {
+				"txtName": {
+					"required": {
+						"msg": "Ingrese un valor!"
+					}
+				},
+				"txtLast": {
+					"required": {
+						"msg": "Ingrese un valor!"
+					}
+				},
+				"txtDui": {
+					"required": {
+						"msg": "Ingrese un valor!"
+					},
+					"pattern": {
+						"value": /\d{8}\-\d/,
+						"msg": "Ingrese un valor válido!"
+					}
+				},
+				"txtDate": {
+					"required": {
+						"msg": "Ingrese un valor!"
+					}
+				}
+			},
+			"success": function(){
+				alert('Válido');
+			},
+			"invalid": function(){
+				alert('Inválido');
+			}
+		})
 	})
 
 	window.updateTextFields = () => {
